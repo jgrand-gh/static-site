f608349ee55e421066dfcaa5afa00428fb72e4e1
Josh Grand <8271819+jgrand-gh@users.noreply.github.com>
2025-02-26 21:46:47 -0600

    added split_nodes_delimiter method and test cases

--- public/src/textnode.py
+++ public/src/textnode.py
@@ -2,7 +2,7 @@ from enum import Enum
 from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -23,7 +23,7 @@ class TextNode():
     
     def text_node_to_html_node(text_node):
         match (text_node.text_type):
-            case TextType.NORMAL:
+            case TextType.TEXT:
                 return LeafNode(None, text_node.text)
             case TextType.BOLD:
                 return LeafNode("b", text_node.text)
@@ -37,3 +37,28 @@ class TextNode():
                 return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
             case _:
                 raise Exception("text node has no specified TextType")
+    
+    def split_nodes_delimiter(old_nodes, delimiter, text_type):
+        new_nodes = []
+
+        for node in old_nodes:
+            if node.text_type != TextType.TEXT:
+                new_nodes.append(node)
+            else:
+                if delimiter in node.text:
+                    split_nodes = node.text.split(delimiter)
+
+                    if len(split_nodes) % 2 == 0:
+                        raise ValueError(f"Invalid markdown: Unclosed delimiter {delimiter}")
+
+                    for i in range(len(split_nodes)):
+                        if split_nodes[i] == "":
+                            continue
+                        if i % 2 == 0:
+                            new_nodes.append(TextNode(split_nodes[i], TextType.TEXT))
+                        else:
+                            new_nodes.append(TextNode(split_nodes[i], text_type))
+                else:
+                    new_nodes.append(node)
+
+        return new_nodes
